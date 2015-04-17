9a8447fc48525f7f94bc237101eec6c6321344bf
William Yaworsky <will.yaworsky@gmail.com>
2015-04-16 20:12:45 -0400

    Functional

--- rotate-and-change-pic.js
+++ rotate-and-change-pic.js
@@ -8,6 +8,15 @@ var Twit = require('twit');
 
 var T = new Twit(config.twitter);
 
+var writeLog = function(msg) {
+  var p = path.join(ROOT, 'log', 'log.json');
+  fs.readFile(p, function(err, data) {
+    data = JSON.parse(data);
+    data.push(msg);
+    fs.writeFile(p, JSON.stringify(data, undefined, 2));
+  });
+};
+
 fs.exists(imgPath, function loop(exists) {
   if (!exists) {
     T.get('users/show', { screen_name: 'yaworsw' }, function(err, data, res) {
@@ -19,11 +28,11 @@ fs.exists(imgPath, function loop(exists) {
     require('./lib/modifiers/rotate')(imgPath, { degs: config.degs }).then(function() {
       fs.readFile(imgPath, { encoding: 'base64' }, function(err, b64) {
         T.post('account/update_profile_image', { image: b64, skip_status: 1 }, function(err, data, response) {
-          console.log(err)
+
         });
       });
     }, function() {
-      console.log(arguments)
+      writeLog('error' + arguments);
     });
   }
 });
